41c419a805e10f36a0e4b6b7b3256573a6c65c6e
Ashima Arora <aroashi15@yahoo.com>
2025-06-11 07:21:28 -0400

    extract load completed

--- amazon_food_reviews_pipeline/dags/sentiment-etl-dag.py
+++
@@ -1,9 +0,0 @@
-from airflow import DAG
-from airflow.operators.python import PythonOperator
-from datetime import datetime, timedelta
-import pandas as pd
-import os
-import random
-
-def get_reviews_data():
-    pass
--- amazon_food_reviews_pipeline/dags/sentiment_etl_dag.py
+++ amazon_food_reviews_pipeline/dags/sentiment_etl_dag.py
@@ -1,9 +1,39 @@
+"""
 from airflow import DAG
 from airflow.operators.python import PythonOperator
 from datetime import datetime, timedelta
-import pandas as pd
-import os
-import random
 
-def get_reviews_data():
-    pass
+import os
+
+"""
+
+import pandas as pd
+import random
+import kagglehub
+from kagglehub import KaggleDatasetAdapter
+
+
+# Set the path to the file you'd like to load
+reviews_file_path = "Reviews.csv"
+temp_daily_path = '../data/daily_sample.csv'
+
+# Load the latest version
+df = kagglehub.dataset_load(
+  KaggleDatasetAdapter.PANDAS,
+  "snap/amazon-fine-food-reviews",
+  reviews_file_path,
+  # Provide any additional arguments like 
+  # sql_query or pandas_kwargs. See the 
+  # documenation for more information:
+  # https://github.com/Kaggle/kagglehub/blob/main/README.md#kaggledatasetadapterpandas
+)
+
+def extract_daily_sample(df):
+    df_sample = df.sample(n=500, random_state=random.randint(1, 1000))
+    df_sample.to_csv(temp_daily_path, index=False)
+    print(df_sample.head())
+
+#print("First 5 records:\n", df.head())
+
+extract_daily_sample(df)
+
